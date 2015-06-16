449171d94a3c0a3a866a3b864d7cfb9181c80601
Yogesh Malpani <yogesh.k.malpani@gmail.com>
2015-06-16 12:02:28 +0530

    Config setting.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# android-code-style
+Android Code Styles
+================
+
+Android Studio code style settings for Android projects.
+
+
+Installation
+------------
+
+ * Run the `install.sh` script.
+ * Restart Android Studio if it's running.
+ * Open Android Studio Project Settings -> Code Styles, change the code style for the
+   project to the one you want.
---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Installs IntelliJ configs into your user configs.
+
+echo "Installing code style configs..."
+
+for i in $HOME/Library/Preferences/IntelliJIdea* \
+         $HOME/Library/Preferences/IdeaIC* \
+         $HOME/Library/Preferences/AndroidStudio* 
+
+do
+  # Create codestyles directory if it does not exist (AndroidStudio1.2 for example)
+  if [ ! -d "$i/codestyles" ]; then
+    mkdir $i/codestyles
+  fi
+
+  cp -frv $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/configs/* $i/codestyles 2> /dev/null
+done
+
+echo "Done."
+echo ""
+echo "Restart AndroidStudio, go to preferences, and apply 'YoAndroid'."
